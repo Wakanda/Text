ee9b48ffd48b4b5d4d002e7bb26af1044f920a71
Jonathan Argentiero <jonathan.argentiero@gmail.com>
2014-11-21 11:38:13 +0100

    Widget revision
    for wak11

--- designer.js
+++ designer.js
@@ -3,7 +3,53 @@
     Text.setWidth(50);
     Text.setHeight(16);
 
+    Text.addEvents({ 
+        'name':'action' 
+    },{ 
+        'name':'click', 
+        'category':'Mouse Events' 
+    },{ 
+        'name':'dblclick', 
+        'category':'Mouse Events'
+    },{ 
+        'name':'mousedown', 
+        'category':'Mouse Events'
+    },{ 
+        'name':'mouseout',
+         'category':'Mouse Events'
+    },{ 
+        'name':'mouseover',
+        'category':'Mouse Events'
+    },{ 
+        'name':'mouseup',
+        'category':'Mouse Events'
+    },{ 
+        'name':'touchstart',
+        'category':'Touch Events'
+    },{ 
+        'name':'touchend',
+        'category':'Touch Events'
+    },{ 
+        'name':'touchcancel',
+        'category':'Touch Events'
+    });
+
     Text.customizeProperty('value', { multiline: true });
+
+    Text.addLabel({ defaultValue: '' });
+
+    Text.setPanelStyle({
+        'fClass': true, //This property is for the design panel
+        'text': true,
+        'textShadow': true,
+        'dropShadow': true, 
+        'innerShadow': true,
+        'background': true,
+        'border': true,
+        'sizePosition': true,
+        'label': true,
+        'disabled': ['border-radius']
+    });
 
     var showUrl = function() {
         if(this.url() || this.url.boundDatasource()) {
@@ -16,8 +62,11 @@
     Text.doAfter('init', function() {
         showUrl.call(this);
         this.url.onChange(showUrl);
-        this.subscribe('datasourceBindingChange', 'url', showUrl, this);
+        this.subscribe('datasourceBindingChange', 'url', showUrl, this);  
+    });
+    
+    Text.studioOnResize(function(){
+        this.autoResize(false);
     });
 
-    Text.addLabel({ defaultValue: '' });
 });
--- widget.js
+++ widget.js
@@ -3,9 +3,21 @@ WAF.define('Text', ['waf-core/widget'], function(Widget) {
 
     var Text = Widget.create('Text', {
         value: Widget.property({
-            defaultValueCallback: function() {
+            defaultValueCallback: function(plainText) {
+                if(plainText){
+                    return this.node.textContent;
+                }
                 return this.node.innerHTML;
             }
+        }),
+        format: Widget.property({
+            type: 'string',
+            bindable: false
+        }),
+        autoResize: Widget.property({
+            type: 'boolean',
+            defaultValue: true,
+            bindable: false
         }),
         url: Widget.property({ type: 'string' }),
         urlTarget: Widget.property({
@@ -18,17 +30,85 @@ WAF.define('Text', ['waf-core/widget'], function(Widget) {
             defaultValue: true,
             bindable: false
         }),
-        render: function() {
+        scrollbar: Widget.property({
+            type: 'enum',
+            values: {
+                'hidden':  'Hidden',
+                'horizontal':  'Horizontal',
+                'vertical': 'Vertical',
+                'both': 'Both'
+            },
+            defaultValue: 'Hidden',
+            bindable: false
+        }),
+        render: function(value) {
+            value = value || this.value();
+            value = WAF.utils.formatString(value,this.format());
             if(this.plainText()) {
-                this.node.textContent = this.value();
+                this.node.textContent = value;
             } else {
-                this.node.innerHTML = this.value();
+                this.node.innerHTML = value;
             }
+            this.autoResizer();
+        },
+        clear: function(){
+            this.value('');
+        },
+        autoResizer: function(){
+            if (this.autoResize()) {
+                this.style({
+                    'width'     : 'auto',
+                    'height'    : 'auto',
+                    'white-space': 'nowrap'
+                });
+                this.size(this.width(),this.height());
+                this.autoResize(true);
+            }else{
+                this.style({
+                    'width'     : '',
+                    'height'    : '',
+                    'white-space': 'normal'
+                });
+            }
+        },
+        setOverflow: function(){
+            switch(this.scrollbar()){
+                case 'hidden':
+                    this.style({
+                        'overflow'  : 'hidden'
+                    });
+                    break;
+                case 'horizontal':
+                    this.style({
+                        'overflow-x'  : 'auto',
+                        'overflow-y'  : 'hidden'
+                    });
+                    break;
+                case 'vertical':
+                    this.style({
+                        'overflow-x'  : 'hidden',
+                        'overflow-y'  : 'auto'
+                    });
+                    break;
+                case 'both':
+                    this.style({
+                        'overflow'  : 'auto'
+                    });
+                    break;
+                default:
+                    break;
+            };
         },
         init: function() {
             this.render();
-            this.value.onChange(this.render);
-            this.plainText.onChange(this.render);
+            this.autoResizer();
+            this.setOverflow();
+
+            this.value.onChange(function(){ this.render(); });
+            this.plainText.onChange(function(){ this.render(); });
+            this.format.onChange(function(){ this.render(); });
+            this.autoResize.onChange(function(){ this.autoResizer(); });
+            this.scrollbar.onChange(function(){ this.setOverflow(); });
 
             $(this.node).on('click', function() {
                 if(this.url()) {
